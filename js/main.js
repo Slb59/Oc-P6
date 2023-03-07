2c0c304ddb7915b740f2fb1867486c566261670e
Slb59 <sylvie.bricout2@laposte.net>
2023-03-07 20:23:49 +0100

    Add carroussel best

--- js/main.js
+++ js/main.js
@@ -22,39 +22,73 @@ function show_best_movie(){
 
 show_best_movie();
 
-function show_image(category, image_id) {
-    bestMovies = fetch_url(BEST_MOVIES_URL);
+function show_image(page, category, image_id, image_num) {
+    urlPage = BEST_MOVIES_URL + "&page=" + page
+    console.log(urlPage)
+    bestMovies = fetch_url(urlPage);
     console.log(bestMovies)
     bestMovies.then(value => {
-        console.log(value.results[image_id].url)
-        bestMovies_data = fetch_url(value.results[image_id-1].url);
+        bestMovies_data = fetch_url(value.results[image_id].url);
         bestMovies_data.then(movie_data => {
-            let image_selector = ".category_" + category + " > " + ".movie_elem_" + image_id
+            let image_selector = ".category_" + category + " .movie_item_" + image_num
             let img = document.createElement('img');
             img.setAttribute('src',  movie_data.image_url);
-            img.setAttribute('alt', "image_" + image_id);
+            img.setAttribute('alt', "image_" + image_num);
             document.querySelector(image_selector).appendChild(img);
         })
     });
 }
 
-let caroussel_list = ["best", "adventure", "animation", "biography"]
-let caroussel_pos = [1, 1, 1, 1]
-for (let category of caroussel_list) {
+// const CAROUSSEL_LIST = ["best", "adventure", "animation", "biography"]
+const CAROUSSEL_LIST = ["best"]
+let carousselPositions = [0, 0, 0, 0]
+for (let category of CAROUSSEL_LIST) {
     console.log(category)
-    for (let image_pos=1; image_pos <=4; image_pos++) {
-        show_image(category, image_pos);
+    let image_num = 1
+    for (let imagePos=0; imagePos <5; imagePos++) {
+        show_image(1, category, imagePos, image_num);
+        image_num++;
+    }
+    for (let imagePos=0; imagePos <2; imagePos++) {
+        show_image(2, category, imagePos, image_num);
+        image_num++
+    }    
+}
+
+
+function scroll_caroussel(category, direction) {
+    let carousselNum = 0 // 0 for the best category    
+    console.log(category + "->" + direction)    
+    let containerToScroll = document.querySelector(".category_" + category)
+    // ?????????????????????????????????????????
+    let head= document.querySelector('body')
+    let movieWidth = getComputedStyle(head).getPropertyValue('--movie_width')
+    console.log(movieWidth)
+    let scrollWidth = 182 + 5
+    // scroll the container
+    if (direction === "left") {
+        containerToScroll.scroll({left: -(scrollWidth), top:0, behavior: 'smooth'})
+        carousselPositions[carousselNum]  -= scrollWidth
+    }
+    else if (direction === "right") {
+        containerToScroll.scroll({left: (scrollWidth), top:0, behavior: 'smooth'})
+        carousselPositions[carousselNum]  += scrollWidth
+    }
+    // set visibility of buttons
+    let carousselPos = carousselPositions[carousselNum]
+    console.log(carousselPos)
+    console.log(containerToScroll.scrollWidth)
+    console.log(containerToScroll.clientWidth)
+    console.log(containerToScroll.offsetWidth)
+    let leftButton = document.getElementsByClassName('left_button')[carousselNum]
+    let rightButton = document.getElementsByClassName('right_button')[carousselNum]
+    if (carousselPos === 0) {
+        leftButton.style.visibility = "hidden";
+    } else if (carousselPos > 0 && carousselPos < containerToScroll.scrollWidth) {
+        leftButton.style.visibility = "visible";
+        rightButton.style.visibility = "visible";
+    } else if (carousselPos === containerToScroll.scrollWidth) {
+        rightButton.style.visibility = "hidden";
     }
 }
 
-function scroll_caroussel(category, direction) {
-    console.log(category + "->" + direction)
-    container_to_scroll = document.querySelector(".category_" + category)
-    let scroll_width = $movie_width + $movie_space_between
-    if (direction == "left") {
-        container_to_scroll.scroll({left:400, top:0, behavior: 'smooth'})
-    }
-    else if (direction == "right") {
-        // scroll
-    }
-}
