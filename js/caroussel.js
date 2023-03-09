9859ab2b32e77a6752692274cd97199e8ea224d9
Slb59 <sylvie.bricout2@laposte.net>
2023-03-09 19:11:28 +0100

    Add modal

---
+++ js/caroussel.js
@@ -0,0 +1,109 @@
+class Caroussel {
+    constructor(category, title) {
+        this.category = category
+        this.title = title
+        this.position = 0
+
+        this.scrollWidth = 182 + 5; // TODO : Comment utiliser les variables css ?
+        // ?????????????????????????????????????????
+        // let head= document.querySelector('body');
+        // let movieWidth = getComputedStyle(head).getPropertyValue('--movie_width');
+        // console.log(movieWidth);
+
+        // container are refreshed by htmlGenerator
+        this.container = document.querySelector(".caroussel_" + category)
+        this.containerToScroll = document.querySelector(".category_" + category)
+        this.leftButton = document.querySelector(".caroussel_" + category + " > .left_button")
+        this.rightButton = document.querySelector(".caroussel_" + category + " > .right_button")
+    }
+
+    load_image(page, image_id, image_num) {
+        let urlPage = BEST_MOVIES_URL + "&page=" + page + "&genre=" + this.category.replace('best','')
+        console.log(urlPage)
+        bestMovies = fetchUrl(urlPage);
+        console.log(bestMovies)
+        bestMovies.then(value => {
+            let bestMovies_data = fetchUrl(value.results[image_id].url);
+            bestMovies_data.then(movie_data => {
+                let image_selector = ".category_" + this.category + " .movie_item_" + image_num
+                let img = document.createElement('img');
+                img.setAttribute('src',  movie_data.image_url);
+                img.setAttribute('alt', "image_" + image_num);
+                // TODO : revoir le fonctionnement de appendChild
+                document.querySelector(image_selector).appendChild(img);
+            })
+        });
+    }
+    
+    load_all_images() {
+        let image_num = 1;
+        for (let imagePos=0; imagePos <5; imagePos++) {
+            this.load_image(1, imagePos, image_num);
+            image_num++;
+        }
+        // Read the next page ?
+        for (let imagePos=0; imagePos <2; imagePos++) {
+            this.load_image(2, imagePos, image_num);
+            image_num++;
+        }    
+    }
+
+    setButtonVisibiltity() {
+        console.log(this.position);
+        console.log(this.containerToScroll.scrollWidth);
+        console.log(this.containerToScroll.clientWidth);
+        console.log(this.containerToScroll.offsetWidth);
+        // let leftButton = document.getElementsByClassName('left_button')[carousselNum]
+        // let rightButton = document.getElementsByClassName('right_button')[carousselNum]
+        let maxScrollWidth = this.containerToScroll.scrollWidth - this.containerToScroll.clientWidth
+        console.log(this.position + this.scrollWidth)
+        console.log(maxScrollWidth)
+        if (this.position === 0) {
+            this.leftButton.style.visibility = "hidden";
+        } else if (this.position > 0 && this.position + this.scrollWidth <= maxScrollWidth) {
+            this.leftButton.style.visibility = "visible";
+            this.rightButton.style.visibility = "visible";
+        } else if (this.position + this.scrollWidth >= maxScrollWidth) {
+            this.rightButton.style.visibility = "hidden";
+        }
+    }
+
+    scrollTo(direction) {
+        // scroll the container
+        if (direction === "left") {
+            this.containerToScroll.scroll({left: this.position-(this.scrollWidth), top:0, behavior: 'smooth'});
+            this.position  -= this.scrollWidth;
+        }
+        else if (direction === "right") {
+            console.log('vers la droite')
+            this.containerToScroll.scroll({left: this.position + (this.scrollWidth), top:0, behavior: 'smooth'});
+            this.position  += this.scrollWidth;
+        }
+        // set visibility of buttons
+        this.setButtonVisibiltity();
+
+    }
+
+    htmlGenerator() {
+        let htmlToAdd = "<h2>" + this.title + "</h2>"
+        htmlToAdd += "<a class=\"left_button\" onclick=\"scroll_caroussel('" + this.category + "', 'left')\">";
+        htmlToAdd += "<img src=\"assets/flèche-left.png\" alt=\"fleche\" width=\"100px\">";
+        htmlToAdd += "</a>";
+        htmlToAdd += "<div class=\"category_" + this.category + " category_movies\">";
+        htmlToAdd += "<div class=\"movie_item_1\"></div>";
+        htmlToAdd += "<div class=\"movie_item_2\"></div>";
+        htmlToAdd += "<div class=\"movie_item_3\"></div>";
+        htmlToAdd += "<div class=\"movie_item_4\"></div>";
+        htmlToAdd += "<div class=\"movie_item_5\"></div>";
+        htmlToAdd += "<div class=\"movie_item_6\"></div>";
+        htmlToAdd += "<div class=\"movie_item_7\"></div>";
+        htmlToAdd += "</div>";
+        htmlToAdd += "<a class=\"right_button\" onclick=\"scroll_caroussel('" + this.category + "', 'right')\">";
+        htmlToAdd += "<img src=\"assets/flèche-right.png\" alt=\"fleche\" width=\"100px\">";
+        htmlToAdd += "</a>";
+        this.container.innerHTML = htmlToAdd;
+        this.containerToScroll = document.querySelector(".category_" + this.category);
+        this.leftButton = document.querySelector(".caroussel_" + this.category + " > .left_button")
+        this.rightButton = document.querySelector(".caroussel_" + this.category + " > .right_button")
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,135 +1,30 @@
 const URL_API = "http://localhost:8000/api/v1/titles/"
 const BEST_MOVIES_URL = URL_API + "?sort_by=-imdb_score"
 
-function fetch_url(url) {
+// TODO : may be converted to async function ??
+function fetchUrl(url) {
     return fetch(url)
     .then(response => response.json())
     .catch(err => console.log("Problem with fetch:" + err));
 }
 
 function show_best_movie(){
-    bestMovies = fetch_url(BEST_MOVIES_URL);
+    bestMovies = fetchUrl(BEST_MOVIES_URL);
     bestMovies.then(value => {
-        bestMovies_data = fetch_url(value.results[0].url);
+        bestMovies_data = fetchUrl(value.results[0].url);
         bestMovies_data.then(movie_data => {
             document.getElementById("best_movie_title").innerHTML = movie_data.title
             document.getElementById("best_movie_summary").innerHTML = movie_data.description
             document.getElementById("best_movie_img").src = movie_data.image_url
+            document.getElementById("play_btn").addEventListener(
+                "click", function()  {showModal(movie_data);}
+                );            
         }
             )
     });
 }
 
 show_best_movie();
-
-class Caroussel {
-    constructor(category, title) {
-        this.category = category
-        this.title = title
-        this.position = 0
-        
-        this.scrollWidth = 182 + 5; // TODO : Comment utiliser les variables css ?
-        // ?????????????????????????????????????????
-        // let head= document.querySelector('body');
-        // let movieWidth = getComputedStyle(head).getPropertyValue('--movie_width');
-        // console.log(movieWidth);
-
-        // container are refreshed by htmlGenerator
-        this.container = document.querySelector(".caroussel_" + category)
-        this.containerToScroll = document.querySelector(".category_" + category)
-        this.leftButton = document.querySelector(".caroussel_" + category + " > .left_button")
-        this.rightButton = document.querySelector(".caroussel_" + category + " > .right_button")
-    }
-
-    load_image(page, image_id, image_num) {
-        let urlPage = BEST_MOVIES_URL + "&page=" + page + "&genre=" + this.category.replace('best','')
-        console.log(urlPage)
-        bestMovies = fetch_url(urlPage);
-        console.log(bestMovies)
-        bestMovies.then(value => {
-            let bestMovies_data = fetch_url(value.results[image_id].url);
-            bestMovies_data.then(movie_data => {
-                let image_selector = ".category_" + this.category + " .movie_item_" + image_num
-                let img = document.createElement('img');
-                img.setAttribute('src',  movie_data.image_url);
-                img.setAttribute('alt', "image_" + image_num);
-                // TODO : revoir le fonctionnement de appendChild
-                document.querySelector(image_selector).appendChild(img);
-            })
-        });
-    }
-    
-    load_all_images() {
-        let image_num = 1;
-        for (let imagePos=0; imagePos <5; imagePos++) {
-            this.load_image(1, imagePos, image_num);
-            image_num++;
-        }
-        for (let imagePos=0; imagePos <2; imagePos++) {
-            this.load_image(2, imagePos, image_num);
-            image_num++;
-        }    
-    }
-
-    setButtonVisibiltity() {
-        console.log(this.position);
-        console.log(this.containerToScroll.scrollWidth);
-        console.log(this.containerToScroll.clientWidth);
-        console.log(this.containerToScroll.offsetWidth);
-        // let leftButton = document.getElementsByClassName('left_button')[carousselNum]
-        // let rightButton = document.getElementsByClassName('right_button')[carousselNum]
-        let maxScrollWidth = this.containerToScroll.scrollWidth - this.containerToScroll.clientWidth
-        console.log(this.position + this.scrollWidth)
-        console.log(maxScrollWidth)
-        if (this.position === 0) {
-            this.leftButton.style.visibility = "hidden";
-        } else if (this.position > 0 && this.position + this.scrollWidth <= maxScrollWidth) {
-            this.leftButton.style.visibility = "visible";
-            this.rightButton.style.visibility = "visible";
-        } else if (this.position + this.scrollWidth >= maxScrollWidth) {
-            this.rightButton.style.visibility = "hidden";
-        }
-    }
-
-    scrollTo(direction) {
-        // scroll the container
-        if (direction === "left") {
-            this.containerToScroll.scroll({left: this.position-(this.scrollWidth), top:0, behavior: 'smooth'});
-            this.position  -= this.scrollWidth;
-        }
-        else if (direction === "right") {
-            console.log('vers la droite')
-            this.containerToScroll.scroll({left: this.position + (this.scrollWidth), top:0, behavior: 'smooth'});
-            this.position  += this.scrollWidth;
-        }
-        // set visibility of buttons
-        this.setButtonVisibiltity();
-
-    }
-
-    htmlGenerator() {
-        let htmlToAdd = "<h2>" + this.title + "</h2>"
-        htmlToAdd += "<a class=\"left_button\" onclick=\"scroll_caroussel('" + this.category + "', 'left')\">";
-        htmlToAdd += "<img src=\"assets/flèche-left.png\" alt=\"fleche\" width=\"100px\">";
-        htmlToAdd += "</a>";
-        htmlToAdd += "<div class=\"category_" + this.category + " category_movies\">";
-        htmlToAdd += "<div class=\"movie_item_1\"></div>";
-        htmlToAdd += "<div class=\"movie_item_2\"></div>";
-        htmlToAdd += "<div class=\"movie_item_3\"></div>";
-        htmlToAdd += "<div class=\"movie_item_4\"></div>";
-        htmlToAdd += "<div class=\"movie_item_5\"></div>";
-        htmlToAdd += "<div class=\"movie_item_6\"></div>";
-        htmlToAdd += "<div class=\"movie_item_7\"></div>";
-        htmlToAdd += "</div>";
-        htmlToAdd += "<a class=\"right_button\" onclick=\"scroll_caroussel('" + this.category + "', 'right')\">";
-        htmlToAdd += "<img src=\"assets/flèche-right.png\" alt=\"fleche\" width=\"100px\">";
-        htmlToAdd += "</a>";
-        this.container.innerHTML = htmlToAdd;
-        this.containerToScroll = document.querySelector(".category_" + this.category);
-        this.leftButton = document.querySelector(".caroussel_" + this.category + " > .left_button")
-        this.rightButton = document.querySelector(".caroussel_" + this.category + " > .right_button")
-    }
-}
 
 const CAROUSSEL_LIST = ["best", "adventure", "animation", "biography"]
 const CAROUSSEL_TITLES = [
@@ -156,3 +51,30 @@ function scroll_caroussel(category, direction) {
     if (category === "biography") {carousselList[3].scrollTo(direction);}
 }
 
+function showModal(movie_data) {
+    console.log(movie_data);
+    // http://localhost:8000/api/v1/titles/499549
+    document.querySelector(".movie_modal").style.display = "block";
+    document.querySelector(".movie_title").innerHTML = "<h2>" + movie_data.title + "</h2>";
+    htmlToAdd = "<img src=\"" + movie_data.image_url + "\" alt=\"image\">";
+    document.querySelector(".movie_img").innerHTML = htmlToAdd;
+    document.querySelector(".movie_description").innerHTML = "<p>" + movie_data.description + "</p>";
+    document.querySelector(".movie_genre").innerHTML = "<p>Genres: " + movie_data.genres + "</p>";
+    // TODO : show the french date format
+    document.querySelector(".movie_published").innerHTML = "<p>Sortie: " + movie_data.date_published + "</p>";
+    // ou vote ? ou avg_vote ?
+    document.querySelector(".movie_rated").innerHTML = "<p>Note: " + movie_data.rated + "</p>";
+    document.querySelector(".movie_imdb").innerHTML = "<p>Score imdb: " + movie_data.imdb_score + "</p>";
+    document.querySelector(".movie_directors").innerHTML = "<p>Réalisateurs: " + movie_data.directors + "</p>";
+    // use a grid 
+    document.querySelector(".movie_actors").innerHTML = "<p>Acteurs: " + movie_data.actors + "</p>";
+    // unité ??
+    document.querySelector(".movie_duration").innerHTML = "<p>Durée: " + movie_data.duration + "</p>";
+    document.querySelector(".movie_countries").innerHTML = "<p>Pays: " + movie_data.countries + "</p>";
+    if (movie_data.worldwide_gross_income == null) {
+        document.querySelector(".movie_income").innerHTML = "<p>Recettes non connues</p>";
+    } else {    
+        document.querySelector(".movie_income").innerHTML = 
+            "<p>Recettes: " + movie_data.worldwide_gross_income.toString() + movie_data.budget_currency + "</p>";
+    }
+}
